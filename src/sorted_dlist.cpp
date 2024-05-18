57d6f516d36be6750c8ad9f60f52cf26ec929006
DejaSenti <60943224+DejaSenti@users.noreply.github.com>
2024-05-18 20:10:03 +0300

    implemented dlist

--- src/sorted_dlist.cpp
+++ src/sorted_dlist.cpp
@@ -1,18 +1,27 @@
+#include <algorithm>
+
 #include "sorted_dlist.hpp"
 
 void SortedDList::Insert(std::vector<int> sequence)
 {
-
+    for (auto num : sequence)
+    {
+        auto const position = std::lower_bound(m_dlist.begin(), m_dlist.end(), num);
+        m_dlist.insert(position, num);
+    }
 }
 
 void SortedDList::Remove(std::vector<int> indices)
 {
-
+    for (auto num : indices)
+    {
+        m_dlist.erase(std::next(m_dlist.begin(), num));
+    }
 }
 
 bool SortedDList::IsSorted(void) const
 {
-    return false;
+    return std::is_sorted(m_dlist.begin(), m_dlist.end());
 }
 
 std::vector<int> SortedDList::GetContents(void) const
